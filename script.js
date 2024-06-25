979071b4e4d172586d6281bbb5fec2da9c415b33
Arpit Dalal <arpitdalal90@gmail.com>
2024-06-25 10:12:29 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,12 @@
+var Typed = new Typed(".typedText", {
+    strings: [
+        "learning React JS",
+        "doing DSA practice",
+        "a Web Developer",
+        "solving leetcode problems",
+    ],
+    loop: true,
+    typeSpeed: 80,
+    backSpeed: 60,
+    backDelay: 1000,
+});
